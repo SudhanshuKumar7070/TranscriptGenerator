bd528d21b27db37ed7799e28523ff65b275a9979
sk <you@example.com>
2026-08-19 02:02:52 +0530

    fix:url

--- SubtitleMakerClient/client/src/services/transcription.service.ts
+++ SubtitleMakerClient/client/src/services/transcription.service.ts
@@ -1,4 +1,4 @@
-const API_URL = ' https://transcriptgenerator-backend.onrender.com/api/v1';
+const API_URL = 'https://transcriptgenerator-backend.onrender.com/api/v1';
 
 export const transcriptionService = {
   async transcribeVideo(file: File): Promise<string> {
